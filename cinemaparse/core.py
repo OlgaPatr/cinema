a8401ab246d7730cb30de49575a2bb58c373a18e
OlgaPatr <45653348+OlgaPatr@users.noreply.github.com>
2019-10-28 14:17:19 +0300

    Update core.py

--- cinemaparse/core.py
+++ cinemaparse/core.py
@@ -1,21 +1,19 @@
+'''Cinema module'''
 import requests
-from bs4 import BeautifulSoup
-
 class CinemaParser:
-  def __init__(self,town='msk'):
-    self.city = town
-  
-  def extract_raw_content(self):
-     inf = requests.get('https://{}.subscity.ru/'.format(self.city))
-     self.content = inf.text
-    
-  def print_raw_content(self):
-    soup = BeautifulSoup(self.content)
-    print(soup.prettify())
-    
-spb_parser = CinemaParser('spb')
-spb_parser.extract_raw_content()
-spb_parser.print_raw_content()
-#msk_parser = CinemaParser('msk')
-#extract_raw_content()
-#print_raw_content()
+    '''Class CinemaParser '''
+    def __init__(self, town='msk'):
+        '''Выбор города'''
+        self.city = town
+        self.content = []
+    def extract_raw_content(self):
+        '''Доставание информации со страницы'''
+        inf = requests.get('https://{}.subscity.ru/'.format(self.city))
+        self.content = inf.text
+    def print_raw_content(self):
+        '''Вывод информации со страницы'''
+        soup = BeautifulSoup(self.content)
+        print(soup.prettify())
+SPB_PARSER = CinemaParser('spb')
+SPB_PARSER.extract_raw_content()
+SPB_PARSER.print_raw_content()
